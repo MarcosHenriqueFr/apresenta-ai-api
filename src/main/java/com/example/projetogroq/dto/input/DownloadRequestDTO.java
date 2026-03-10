668cca5c6f3e3d915fb408a5780107e08079a761
MarcosHenriqueFr <marcosfhtrabalho@gmail.com>
2026-03-10 11:35:57 -0300

    fix: remove a lógica de estilização do slide da primeira requisição

---
+++ src/main/java/com/example/projetogroq/dto/input/DownloadRequestDTO.java
@@ -0,0 +1,7 @@
+package com.example.projetogroq.dto.input;
+
+import com.example.projetogroq.dto.SlideStyle;
+
+public record DownloadRequestDTO(
+        SlideStyle style
+) { }
--- src/main/java/com/example/projetogroq/dto/input/PresentationRequestDTO.java
+++ src/main/java/com/example/projetogroq/dto/input/PresentationRequestDTO.java
@@ -2,11 +2,11 @@ package com.example.projetogroq.dto.input;
 
 import com.example.projetogroq.dto.OutputQuality;
 import com.example.projetogroq.dto.SlideLevel;
+import com.example.projetogroq.dto.SlideStyle;
 
 public record PresentationRequestDTO(
     String topic,
     int durationInMinutes,
     SlideLevel level,
-    OutputQuality quality,
-    String style
+    OutputQuality quality
 ) { }
