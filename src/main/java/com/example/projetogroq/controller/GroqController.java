3c41d0c23c232a7da121ab32eda8e9e5656acc4b
MarcosHenriqueFr <marcosfhtrabalho@gmail.com>
2026-03-10 11:36:26 -0300

    fix: modifica o mapeamento de endpoints da api

--- src/main/java/com/example/projetogroq/controller/GroqController.java
+++ src/main/java/com/example/projetogroq/controller/GroqController.java
@@ -3,6 +3,10 @@ package com.example.projetogroq.controller;
 import com.example.projetogroq.dto.input.PresentationRequestDTO;
 import com.example.projetogroq.dto.output.PresentationResponseDTO;
 import com.example.projetogroq.service.GroqService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,8 +14,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/presentation")
 public class GroqController {
+
+    private static final Logger logger = LoggerFactory.getLogger(GroqController.class);
 
     private final GroqService groqService;
 
@@ -20,8 +26,14 @@ public class GroqController {
     }
 
     @PostMapping("/generate")
-    public ResponseEntity<PresentationResponseDTO> generateSlides(@RequestBody PresentationRequestDTO dto){
-        PresentationResponseDTO response = groqService.generatePresentation(dto);
+    public ResponseEntity<PresentationResponseDTO> generateSlides(HttpServletRequest request, @RequestBody PresentationRequestDTO dto){
+        HttpSession session = request.getSession(true);
+
+        PresentationResponseDTO response = groqService.generatePresentation(session, dto);
+
+        logger.info("Sessão criada: {}", session.getId());
+        logger.info("Apresentação salva na sessão com sucesso.");
+
         return ResponseEntity.ok(response);
     }
 }
